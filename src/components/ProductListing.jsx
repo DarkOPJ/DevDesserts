92dd793dab4fbe3805795a5c95098fb3d07142c8
Boadu Aaron Jeffrey Bosomepem <jeffreyaaron84@gmail.com>
2024-12-10 14:28:20 +0000

    Added code in product listings to handle errors when fetching from the online json server.

--- src/components/ProductListing.jsx
+++ src/components/ProductListing.jsx
@@ -1,30 +1,43 @@
-import React from 'react'
-import Product from './Product'
-import LoadingSpinner from './LoadingSpinner'
+import React from "react";
+import Product from "./Product";
+import LoadingSpinner from "./LoadingSpinner";
 import { useState, useEffect } from "react";
 
-const ProductListing = ({products}) => {
+const ProductListing = ({ products }) => {
   const [productsFetched, setProductsFetched] = useState(false);
   useEffect(() => {
     setProductsFetched(!!products && products.length > 0);
   }, [products]); // Runs whenever 'products' changes
 
-  if(productsFetched){
+  // This is for the online json server api version
+  const processedProducts = products.map((product, index) => ({
+    ...product,
+    id: product.id || index,
+  }));
+  // This is for the online json server api version
+
+  if (productsFetched) {
     return (
       <div className="container grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 gap-10">
-      {
-        products.map((product) => (
-          <Product key={product.id} product={product}/>
-        ))
-      }
-    </div>
-    )
-  }
-  return(
-    <div className="w-full h-52 grid place-items-center">
-        <LoadingSpinner loading={!productsFetched} />
-      </div>
-  )
-}
+        {
+          // products.map((product) => (
+          //   <Product key={product.id} product={product}/>
+          // ))
 
-export default ProductListing
+          // This is for the online json server api version
+          processedProducts.map((product) => (
+            <Product key={product.id} product={product} />
+          ))
+          // This is for the online json server api version
+        }
+      </div>
+    );
+  }
+  return (
+    <div className="w-full h-52 grid place-items-center">
+      <LoadingSpinner loading={!productsFetched} />
+    </div>
+  );
+};
+
+export default ProductListing;
